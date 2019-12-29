b68f9268aa43b04a654c88aa6066e2e040a5727e
Pedro Pinho <rihor1000@gmail.com>
2019-12-29 09:54:00 -0300

    Next configuration

---
+++ routes.js
@@ -0,0 +1,9 @@
+const routes = require("next-routes");
+
+module.exports = routes()
+	.add("/", "home")
+	.add("/sign", "sign")
+	.add("/profile/:id", "profile")
+	.add("/wallpaper/create", "create")
+	.add("/wallpaper/:id", "wallpaper")
+	.add("/wallpaper/:id/edit", "edit");
