0d9bcf14b1699084f702d00870b132d87351d754
Pedro Pinho <rihor1000@gmail.com>
2020-02-13 19:47:36 -0300

    Updating Next Adding PWA config removing unecessary lib

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPlugins = require('next-compose-plugins')
 const images = require('next-images')
 const css = require('@zeit/next-css')
+const offline = require('next-offline')
 
-module.exports = withPlugins([images, css])
+module.exports = withPlugins([images, css, offline])
