5ccbbe9e5526e63a354759b2625001361271cf15
Pedro Pinho <rihor1000@gmail.com>
2020-02-20 12:45:07 -0300

    Revert "Merge remote-tracking branch 'frontend-next/master'"
    This reverts commit 758c76c18f2f6c5113978c71233159dd0bef1921.

--- frontend/next.config.js
+++
@@ -1,6 +0,0 @@
-const withPlugins = require('next-compose-plugins')
-const images = require('next-images')
-const css = require('@zeit/next-css')
-const offline = require('next-offline')
-
-module.exports = withPlugins([images, css, offline])
--- frontend/pages/wallpaper.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-// import { Container } from './styles';
-
-export default function wallpaper() {
-  return <div />
-}
