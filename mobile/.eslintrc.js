50404ed1fd1ab30b45967fce3dad3ebe0213882d
Pedro Pinho <rihor1000@gmail.com>
2020-02-21 13:21:42 -0300

    ESLint and Prettier config

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+    'prettier/react',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off'
+  },
+};
